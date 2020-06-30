e213ca8eec5eb5ec8c513846539a977c4c5dd7ab
aysha.babur <aysha.babur@news.co.uk>
2020-06-30 12:58:44 +0100

    third commit

--- src/main/main.go
+++ src/main/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	. "github.com/baburay23/git-Backend-/src/http"
-
+	. "github.com/baburay23/git-Backend-/src/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,28 +10,25 @@ import (
 //all my get routes are here for each activity 
 //The endpoint (or route) is the url you request for. It follows this structure
 func main(){
+	
+	//fmt.Println(Education{"Learn Elm", 1, "Education", 1, 1, 1 })
+	 //rand.Seed(time.Now().Unix())
+	//education := Education{ Activity: "Learn Rust", Accessibility: 1, Type: "Education",Participants: 1, Price: 1, Key: 1,}
+	//Education.Activity = "Learn Rust"
+	 //fmt.Println("Bored API", Education.Activity)
+		
     r := gin.Default()
 	r.GET("/charity",GetCharity)
     //r.GET("/cooking", GetCooking)
 	r.GET("/education",GetEducation) 
-	
+		
 	//The HandlerFunc type is an cadapter to allow the use of ordinary functions as HTTP handlers.
 	r.Run()
-}
-	
-/* 	Education := []Education { 
-		{ Activity: "Learn Rust",
-		Accessibility: 1,
-		Type: "Education",
-		Participants: 1,
-		Price: 1,
-		Key: 1,
-	 },
   }
-} */
 
 
 	
+
 
 
 
